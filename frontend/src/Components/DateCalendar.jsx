f9771fb2674c54a112a1177cb146772dfe2b0bdb
MyYodaSoda <alexander.connolly@edu.sait.ca>
2024-03-14 07:22:44 -0600

    More page work

--- frontend/src/Components/DateCalendar.jsx
+++ frontend/src/Components/DateCalendar.jsx
@@ -1,11 +1,27 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker";
 import CourseSelection from "./CourseSelection";
 import Container from "@mui/material/Container";
 
-export default function StaticDateTimePickerLandscape() {
+export default function StaticDateTimePickerLandscape(props) {
+  const [value, setValue] = React.useState(null);
+  const onDatePicked = (event) => {
+    setValue(event);
+    let onlyDate = event.$d.toISOString();
+    props.onChange(onlyDate);
+    console.log("Date Changed", event, onlyDate);
+  };
+
+  React.useEffect(() => {
+    if (props.initialValue !== 0 && value === null) {
+      setValue(props.initialValue);
+      console.log("Initial Date is", props.initialValue);
+    }
+  }, [props.initialValue, value, setValue]);
+
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Container style={{ marginBottom: "30px", alignContent: "center" }}>
@@ -19,7 +35,11 @@ export default function StaticDateTimePickerLandscape() {
           gridAutoColumns: "unset",
           backgroundColor: "black",
         }}
-      ></StaticDateTimePicker>
+        label={props.label}
+        value={value}
+        onChange={onDatePicked}
+        renderInput={(params) => <TextField {...params} />}
+      />
     </LocalizationProvider>
   );
 }
